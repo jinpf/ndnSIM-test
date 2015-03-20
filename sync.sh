68a7b0a025525bc8b10d5d013b2c50d3abda5ef2
jinpf <jpflcj@sina.com>
2015-03-20 09:17:55 +0800

    initial commit

---
+++ sync.sh
@@ -0,0 +1,6 @@
+# this script is used for manual sync with the modifyed code
+cp -rfu ../graphs ./
+cp -rfu ../ns-3/scratch/ ns-3/
+cp -rfu ../ns-3/src/ndnSIM/apps ns-3/src/ndnSIM/
+cp -rfu ../ns-3/src/ndnSIM/NFD/daemon ns-3/src/ndnSIM/NFD/
+cp -rfu ../ns-3/src/ndnSIM/NFD/core ns-3/src/ndnSIM/NFD/
