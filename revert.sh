fba22b4145f949000414479b8aef33a577c2581c
jinpf <jpflcj@sina.com>
2015-04-17 16:10:16 +0800

    add trace for tracers to log ndn running status in file

--- revert.sh
+++ revert.sh
@@ -9,3 +9,4 @@ cp -rfu ns-3/scratch/ ../ns-3/
 cp -rfu ns-3/src/ndnSIM/apps ../ns-3/src/ndnSIM/
 cp -rfu ns-3/src/ndnSIM/NFD/daemon ../ns-3/src/ndnSIM/NFD/
 cp -rfu ns-3/src/ndnSIM/NFD/core ../ns-3/src/ndnSIM/NFD/
+cp -rfu ns-3/src/ndnSIM/utils/tracers ../ns-3/src/ndnSIM/utils/
