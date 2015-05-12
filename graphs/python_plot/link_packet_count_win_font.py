4378f5e929971aa90d4d4ac272c94ad32f72ac48
jinpf <jpflcj@sina.com>
2015-05-12 13:18:19 +0800

    add liunx plot version

--- graphs/python_plot/link_packet_count_win_font.py
+++ graphs/python_plot/link_packet_count_win_font.py
@@ -3,7 +3,7 @@
 # @Author: jinpf
 # @Date:   2015-05-06 10:05:27
 # @Last Modified by:   jinpf
-# @Last Modified time: 2015-05-12 13:09:07
+# @Last Modified time: 2015-05-11 16:43:53
 
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
@@ -33,12 +33,13 @@ def re_load(data):
 	return (t,c)
 
 def plot_time_count(pull_data,push_data):
+	font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=17) 
 	line1, = plt.plot(pull_data[0],pull_data[1],'r.',label=u'pull',linewidth=1)
 	line2, = plt.plot(push_data[0],push_data[1],'b+',label=u'push',linewidth=1)
-	# plt.legend(handles=[line1,line2])
-	plt.ylabel(u'链路包个数')
-	plt.xlabel(u'时间(每3秒)')
-	plt.title(u'链路包个数')
+	plt.legend(handles=[line1,line2])
+	plt.ylabel(u'链路包个数', fontproperties=font)
+	plt.xlabel(u'时间(每3秒)', fontproperties=font)
+	plt.title(u'链路包个数', fontproperties=font)
 	plt.axis([0,1000,0,8])
 	plt.show()
 
