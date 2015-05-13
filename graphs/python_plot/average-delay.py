ec42fb909accb698e7401d3a764b2af77cddac03
第肆人称 <jpflcj@sina.com>
2015-05-13 10:54:05 +0800

    add delay average calculate
    add delay average calculate

---
+++ graphs/python_plot/average-delay.py
@@ -0,0 +1,10 @@
+def print_average(fname):
+    with open(fname,'r') as f:
+        f.readline()
+        records = f.readlines()
+        delay = [float(record.strip('\n').split('\t')[1]) for record in records]
+        print sum(delay)/len(delay)
+
+if __name__ == '__main__':
+    print_average('app-data-delay-pull.txt')
+    
